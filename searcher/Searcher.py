76d379e768e7189a08e58c5bad0bee737baf45d6
mlynarz21 <C4L4mit0u$>
2019-11-14 12:47:40 +0000

    sources

--- searcher/Searcher.py
+++ searcher/Searcher.py
@@ -1,13 +1,13 @@
-def print_vulnerability(name, source, func_name, used_sanitizers):
+def print_vulnerability(name, func_name, arg):
     print('{"vulnerability": "', end="")
     print(name + '",')
     print('"source": "', end="")
-    print(source + '",')
+    print(arg[1] if (arg[1] is not None) else "", end="\"\n")
     print('"sink": "', end="")
     print(func_name + '",')
     print('"sanitizer": "', end="")
-    print(used_sanitizers if len(used_sanitizers) else "", end="")
-    print('"\n}')
+    print(arg[2] if (arg[2] is not None) else "", end="")
+    print('"}')
 
 
 class Searcher:
@@ -47,7 +47,7 @@ class Searcher:
             if var not in self.declared_variables:
                 self.declared_variables.append(var)
                 for vuln in self.vulnerabilities:
-                    vuln.variables[var] = True
+                    vuln.variables[var] = (True, var, None)
 
 
     def handle_assign(self, instruction):
@@ -58,12 +58,13 @@ class Searcher:
             # check if any of the variables are tainted or untainted to assign the new variable state
             for vuln in self.vulnerabilities:
                 tainted = False
+                var = None
                 for var in used_vars:
-                    if (var in vuln.variables and vuln.variables[var]) or var in vuln.sources:
+                    if (var in vuln.variables and vuln.variables[var][0]) or var in vuln.sources:
                         # assign the new variable state
                         tainted = True
                         break
-                vuln.variables[var_name] = tainted
+                vuln.variables[var_name] = (tainted, var, None)
 
             # if the targets are not yet in the declared variables add them
             if var_name not in self.declared_variables:
@@ -87,15 +88,17 @@ class Searcher:
                     any_variable_in_sanitizer_is_tainted = False
                     if arg in vuln.variables:
                         any_variable_in_sanitizer_is_tainted = True
-                        vuln.variables[arg] = False
+                        # dont make false, make still tainted just add to sanitz
+                        # vuln.variables[arg][0] = False
+                        vuln.variables[arg] = (vuln.variables[arg][0], vuln.variables[arg][1], func_name)
                     # Append into the used sanitizers, needed for output
-                    if any_variable_in_sanitizer_is_tainted:
-                        vuln.used_sanitizers.append(func_name)
+                    # if any_variable_in_sanitizer_is_tainted:
+                    #     vuln.used_sanitizers.append(func_name)
 
             # check if the func name is a sink, if so, check if the arg is tainted, if so, set it as a vulnerability
             elif func_name in vuln.sinks:
                 for arg in handled_args:
-                    if arg in vuln.variables and vuln.variables[arg]:
-                        print_vulnerability(vuln.name, vuln.source, func_name, vuln.used_sanitizers)
+                    if arg in vuln.variables and vuln.variables[arg][0]:
+                        print_vulnerability(vuln.name, func_name, vuln.variables[arg])
 
         return [func_name] + handled_args
