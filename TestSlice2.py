d051f6e384fbfd15190db9162b72532f556f80fa
mlynarz21 <C4L4mit0u$>
2019-11-20 19:22:15 +0000

    tc1 tc2 tc3

---
+++ TestSlice2.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+from searcher.Vulnerability import Vulnerability
+from searcher.Searcher import Searcher, getStr
+import json
+
+from static_analyzer import file_get_contents
+
+
+class TestSlice1(unittest.TestCase):
+
+    def test_rules(self):
+        parsed_snippet = json.loads(file_get_contents(os.getcwd() + "/tests/slice2/slice2.json"))
+        parsed_rules = json.loads(file_get_contents(os.getcwd() + "/tests/slice2/rules.json"))
+        output = open(os.getcwd() + "/tests/slice2/slice2_rules.out", "r")
+        vulnerabilities = Vulnerability.build_vulnerabilities(parsed_rules)
+        s = Searcher(parsed_snippet['body'], vulnerabilities)
+        self.assertEqual(getStr(s.output), output.read(), "Should be equal")
+        output.close()
+
+    def test_rules2(self):
+        parsed_snippet = json.loads(file_get_contents(os.getcwd() + "/tests/slice2/slice2.json"))
+        parsed_rules = json.loads(file_get_contents(os.getcwd() + "/tests/slice2/rules2.json"))
+        output = open(os.getcwd() + "/tests/slice2/slice2_rules2.out", "r")
+        vulnerabilities = Vulnerability.build_vulnerabilities(parsed_rules)
+        s = Searcher(parsed_snippet['body'], vulnerabilities)
+        self.assertEqual(getStr(s.output), output.read(), "Should be equal")
+        output.close()
+
+    def test_rulesNoVuln(self):
+        parsed_snippet = json.loads(file_get_contents(os.getcwd() + "/tests/slice2/slice2.json"))
+        parsed_rules = json.loads(file_get_contents(os.getcwd() + "/tests/slice2/rulesNoVuln.json"))
+        output = open(os.getcwd() + "/tests/slice2/slice2_rulesNoVuln.out", "r")
+        vulnerabilities = Vulnerability.build_vulnerabilities(parsed_rules)
+        s = Searcher(parsed_snippet['body'], vulnerabilities)
+        self.assertEqual(getStr(s.output), output.read(), "Should be equal")
+        output.close()
+
+if __name__ == '__main__':
+    unittest.main()
